d9958700aee5ebc36bb2d92670cce22e4e1f8e19
kevin_linux <kevin_linux@fake.com>
2025-03-15 19:07:54 +0100

    update

--- CPP07/ex00/swap.hpp
+++ CPP07/ex00/swap.hpp
@@ -1,14 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   A.cpp                                              :+:      :+:    :+:   */
+/*   swap.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kevin <kevin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/14 21:48:07 by kevin             #+#    #+#             */
-/*   Updated: 2025/03/14 22:10:58 by kevin            ###   ########.fr       */
+/*   Created: 2025/03/15 16:49:49 by kevin             #+#    #+#             */
+/*   Updated: 2025/03/15 17:39:45 by kevin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "A.hpp"
+#ifndef SWAP_HPP
+#define SWAP_HPP
 
+template<typename T> void swap(T& first, T& second) {
+    T tmp = first;
+    first = second;
+    second = tmp;
+}
+
+template<typename T> T min(T first, T second) {
+    return (first < second) ? first : second;
+}
+
+template<typename T> T max(T first, T second) {
+    return (first > second) ? first : second;
+}
+
+
+#endif
